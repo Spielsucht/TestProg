e1757e5d579370de93b67b5516e67c8ee174617b
Spielsucht <da.we@gmx.net>
2013-11-28 10:44:38 +0100

    initial commit

---
+++ Emotiv/Emotiv/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emotiv;
+using System.Threading;
+
+namespace Emotiv
+{
+    public partial class Form2 : Form
+    {
+        private EmoEngine engine;
+
+        public Form2()
+        {
+            engine = EmoEngine.Instance;
+            InitializeComponent();
+            initialEmoEventHandler(engine);
+        }
+
+        private void checkDongle()
+        {
+            engine.Connect();
+            EmoState state = new EmoState();
+        }
+
+        private void checkHeadset()
+        {
+
+        }
+
+        private void checkSignal()
+        {
+
+        }
+
+        private void initialEmoEventHandler(EmoEngine engine)
+        {
+            engine.
+        }
+    }
+}
